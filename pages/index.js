8f527928ac5b0f2cb898794cb4f48ba950b8a2db
Allan Somensi <allanrigosomensi7br@gmail.com>
2023-08-14 18:00:08 -0300

    Adiciona o arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
 
-    return <h1>Monique, te amo minha linda!! Se o site ta funcionando manda um ❤️</h1>
+  return <h1>Monique, te amo minha linda!! Se o site ta funcionando manda um ❤️</h1>
 }
 
 export default Home;
